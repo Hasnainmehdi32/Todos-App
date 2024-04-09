067fca3fc09ab7304417f1e35b27671dff59949f
Hasnain312 <165997783+Hasnainmehdi32@users.noreply.github.com>
2024-04-09 10:59:33 +0500

    push my todos app to github

---
+++ maccy.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let todosList = [];
+let condition = true;
+console.log("\n \t wellcome to codewithhasnain - Todo-List Application\n");
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your new task ?",
+        }
+    ]);
+    todosList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-List succesfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more task ? ",
+            default: "False",
+        }
+    ]);
+    condition = addMoreTask.addMore;
+}
+console.log("Your Updated Todo-List", todosList);
